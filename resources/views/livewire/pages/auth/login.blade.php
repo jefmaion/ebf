8fc90d0dc7c6721c1b21c525e5e227d09c6bac1a
unknown <jefmaion@hotmail.com>
2026-07-04 14:59:39 -0300

    wip

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -65,7 +65,5 @@ new #[Layout('layouts.guest')] class extends Component
         </div>
     </form>
 
-    <x-slot:link>
-        <div class="text-center text-secondary mt-3">{{ __("Don't have an account?") }} <a href="{{ route('register') }}" tabindex="-1">{{ __("Signup") }}</a></div>
-    </x-slot:link>
+
 </div>
